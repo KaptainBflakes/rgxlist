100145657d0239092fd81bf23ad27f67f57d1fa0
KaptainBflakes <keatonh99@gmail.com>
2020-03-11 17:50:48 -0700

    Fixed stupid mistake
    There was an obvious bug that would stop name scouring if the regex was too long, even if it wasn't longer than the stem.
    Should output now even if the regex is exactly as large as the name and shouldn't throw exceptions and crash even if there are some partial matches with the regex length larger than the stem length.

--- rgxlist.cpp
+++ rgxlist.cpp
@@ -4,7 +4,6 @@
 //requires std:c++17 to compile. NOTE IF YOU WANT TO MAKE A LIST FROM FILE EXTENSION, USE extlist! THIS DOES NOT HANDLE EXTENSION DATA!
 using namespace std;
 using namespace std::filesystem;
-bool contains(string name,string reg);
 int main(int argc, char ** argv){
 	//argv[1] gives the regex string. argv[2] is the input directory. input is assumed local unless given. use '>' to send output to a file
 	string dir=".\\";//If on linux, change this shit to a "./"
@@ -14,22 +13,24 @@ int main(int argc, char ** argv){
 	string out="";string reg=argv[1];
 	for(auto& entry: directory_iterator(dir)){
 		if(entry.path().has_extension()){
-			if(contains(entry.path().stem().string(),reg)){
+			
+			bool contains;//name analysis
+			string name=entry.path().stem().string();
+			contains=false;
+			for(int i=0;i<name.size();i++){
+				if(name[i]==reg[0]&&name.size()-i>=reg.size()){//if find is possible, meaning same start letter and same character size
+					bool good = true;
+					for(int c=0;c<reg.size();c++){
+						if(reg[c]!=name[c+i]){good=false;break;}//interrupts possible find check if regex does not continue to match.	
+					}
+					if(good){contains=true;}
+				}
+			}			
+			if(contains){
 				out+=entry.path().string();out+="\n";
 			}
 		}
 	}
 	cout<<out;
 	return 0;
-}
-bool contains(string name, string reg){//simple regex function
-	for(int i=0;i<name.size()-reg.size();i++){
-		if(name[i]==reg[0]){//if find is possible
-			for(int c=1;c<reg.size();c++){
-				if(reg[c]!=name[c+i]){break;}//interrupts possible find check if regex does not continue to match.
-				if(c==reg.size()-1){return true;}//returns true if possible find completes check.
-			}
-		}
-	}
-	return false;
 }
