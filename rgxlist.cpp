bae2d6e20f210c7ea390ddf8b7a48e396f8a79c2
KaptainBflakes <keatonh99@gmail.com>
2020-03-11 18:13:50 -0700

    Comments and Simplification
    Simplified the code structure a bit as well as better explained what everything does.

--- rgxlist.cpp
+++ rgxlist.cpp
@@ -7,27 +7,21 @@ using namespace std::filesystem;
 int main(int argc, char ** argv){
 	//argv[1] gives the regex string. argv[2] is the input directory. input is assumed local unless given. use '>' to send output to a file
 	string dir=".\\";//If on linux, change this shit to a "./"
-	if(argc==3){
-		dir=argv[2];
-	}
-	string out="";string reg=argv[1];
+	if(argc==3){dir=argv[2];}//optional search directory dication
+	string out="";string reg=argv[1];//regex dictation
 	for(auto& entry: directory_iterator(dir)){
 		if(entry.path().has_extension()){
-			
-			bool contains;//name analysis
 			string name=entry.path().stem().string();
-			contains=false;
 			for(int i=0;i<name.size();i++){
-				if(name[i]==reg[0]&&name.size()-i>=reg.size()){//if find is possible, meaning same start letter and same character size
-					bool good = true;
-					for(int c=0;c<reg.size();c++){
-						if(reg[c]!=name[c+i]){good=false;break;}//interrupts possible find check if regex does not continue to match.	
+				//checks if a match starting at this index is possible. Checks if this index matches the start of the regex and if there is space in the name left for regex.
+				if(name[i]==reg[0]&&name.size()-i>=reg.size()){
+					//checks if possible match is a complete match.
+					bool good = true;for(int c=0;c<reg.size();c++){if(reg[c]!=name[c+i]){good=false;break;}}//if one char doesn't match, break and go on to the next index.
+					//output string building in case of match find.
+					if(good){out+=entry.path().string();out+="\n";
+						break;//breaks so that then next path can be processed.
 					}
-					if(good){contains=true;}
 				}
-			}			
-			if(contains){
-				out+=entry.path().string();out+="\n";
 			}
 		}
 	}
